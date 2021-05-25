072831ac8b995b99a1ad79ed4a8d60fc70800860
Hoversquid <xhenos76@gmail.com>
2021-05-24 20:50:38 -0500

    Early release changes
    Added a menu system, duplicate files instead of overwrite, changed folder structure, general fixes

--- MLAnimator.py
+++ MLAnimator.py
@@ -5,6 +5,7 @@
     # Using --starting_frame (-sf <number>), frames are selected from the selected number as its starting point. this will add the rest of the higher numbered frames, or a number of frames specified by --frames.
     # Using --reverse (-r), animation will be reversed
     # Using --no_mirror (-nm), animation will not include mirrored file list appending (mirrored file list makes the looping animation seemless, but doubles the file size)
+    # Using --info (-i), the frame information will be added to the file name
 
 from os import listdir, mkdir, path, rename, scandir, getcwd
 from os.path import isfile, join
@@ -74,19 +75,17 @@ class MLAnimator:
         return False
 
     def set_new_values(self):
-        select = input("Set new frame values for %s? (y/n) : " % self.name)
-        if select.strip().lower() in ("y", "yes", ""):
-            values_set = False
-            while not values_set:
-                values_set = self.set_frame_info()
-                if values_set == None:
-                    print("Animation skipped.")
-                    return None
-            return True
-        return None
+        values_set = False
+        while not values_set:
+            values_set = self.set_frame_info()
+            if values_set == None:
+                return None
 
+        return True
+
+    # Checks premade animation settings
     def check_valid_frames(self):
-        if self.starting_frame == None:
+        if self.starting_frame == None: # default starting frame is set from number of frames used
             if self.frames == None:
                 self.starting_frame = 1
                 self.frames = self.length
@@ -94,13 +93,13 @@ class MLAnimator:
         if self.starting_frame >= self.length:
             print("Invalid starting frame; must be below %d." % (self.length))
             return False
-        if self.frames == None:
-            frames = self.length - self.starting_frame
+        if self.frames == None: # default frame amount is all that are available
+            self.frames = self.length - self.starting_frame
             return True
         if self.frames > self.length:
             print("Invalid frame amount; must be below max size of %d." % (self.length))
             return False
-        if self.frames + self.starting_frame > self.length:
+        if self.frames + self.starting_frame - 1 > self.length:
             print("Starting Frame + Frame Amount incompatible with max frame size of %d." % (self.length))
             return False
         if self.starting_frame < 0:
@@ -109,6 +108,7 @@ class MLAnimator:
 
     def set_valid_frames(self):
         frames_ready = False
+        print("\nSetting frame information for %s." % self.name)
         while not frames_ready:
             frames_ready = self.set_new_values()
             if frames_ready == None:
@@ -125,21 +125,22 @@ def process_files():
     parser.add_argument("-da", "--dont_animate", action="store_true", help="turn off ffmpeg animation")
     parser.add_argument("-r", "--reverse", action="store_true", help="turn off ffmpeg animation")
     parser.add_argument("-nm", "--no_mirror", action="store_true", help="turn off appending mirrored animation")
-
+    parser.add_argument("-i", "--info", action="store_true", help="add frame info to filename")
     args = parser.parse_args()
     dir = args.dir
-    framerate = args.framerate
+    framerate = int(args.framerate)
     starting_frame = args.starting_frame
     frames = args.frames
     filetype = args.filetype
     animate = not args.dont_animate
     reverse = args.reverse
     mirror_list = not args.no_mirror
+    info = args.info
 
     animator_output_path = path.join(getcwd(), "AnimatorOutput")
 
     if not path.exists(animator_output_path):
-        print("Creating output folder at " + str(animator_output_path))
+        print("Creating main output folder at " + str(animator_output_path))
         mkdir(animator_output_path)
 
     if starting_frame:
@@ -157,42 +158,58 @@ def process_files():
     # get list of files in the selected directory if they are a compatible file type
     files = [f.name for f in scandir(dir) if isfile(join(dir, f.name)) and f.name.split(".")[-1] in image_file_types]
 
-    if len(files) > 0:
+    if len(files) > 1:
         new_dirs = []
         file_listing = []
         collected_files = []
         nametomatch = get_filename(files[0])
-        collected_files.append(files[0])
+        fi = 1
+        if not nametomatch:
+            for f in files:
+                if get_filename(f):
+                    nametomatch = f
+                    collected_files.append(f)
+                    break
+                fi += 1
+        if fi == len(files): return
 
-        # sorts files by file name into separate folders
-        for f in files[1:]:
+        # Sorts the files by their file names. Creates directory for files if there is more than one numbered image with the filename.
+        fAmt = len(files)
+        for i, f in enumerate(files[fi:], start=1):
             checkedname = get_filename(f)
-            if nametomatch == checkedname:
+
+            matched = nametomatch == checkedname
+            last = i + 1 == fAmt
+            if matched:
                 collected_files.append(f)
-            elif checkedname != None:
-                file_listing.append(collected_files)
-                new_dirs.append(nametomatch)
+
+            if not matched or last:
+                if len(collected_files) > 1:
+                    file_listing.append(collected_files)
+                    new_dirs.append(nametomatch)
+
+                # can't think of how to not have this done at the end without more branches
                 nametomatch = checkedname
                 collected_files = [f]
-
-        # append last file set
-        file_listing.append(collected_files)
-        new_dirs.append(nametomatch)
 
         # Get all directories to check if they are sorted image folders
         dirs = [f for f in scandir(dir) if f.is_dir()]
 
-        # If this is the sorted folder, do not sort again. Produces an animation file in the current directory and stops.
+        # If this is the sorted folder, do not sort again. Produces an animation file in its base directory's output folder
         if len(dirs) == 0 and len(new_dirs) == 1:
+            diroutname = path.basename(path.dirname(path.dirname(dir))) + "_" + filetype + "_output"
+
             if animate:
-                outpath = path.join(animator_output_path, new_dirs[0] + "." + filetype)
-                create_animation_file(dir, new_dirs[0], framerate, frames, filetype, starting_frame, mirror_list, reverse, outpath)
+                diroutpath = path.join(animator_output_path, diroutname)
+                if not path.exists(diroutpath):
+                    print("Creating sorted folder for " + str(diroutpath))
+                    mkdir(diroutpath)
+                create_animation_file(dir, new_dirs[0], framerate, frames, filetype, starting_frame, mirror_list, reverse, diroutpath, info)
                 return
 
-            print("Files are in sorted folder and no action taken.")
-
         # If this folder contains more than one set of images, it will sort them into folders.
-        else:
+        if len(new_dirs) > 1:
+            diroutname = path.basename(dir) + "_" + filetype + "_output"
             dirrange = range(len(new_dirs))
             for i in dirrange:
                 newpath = path.join(str(dir), str(new_dirs[i]))
@@ -203,18 +220,22 @@ def process_files():
                     filepath = path.join(dir, f)
                     rename(filepath, path.join(newpath, f))
 
-    # Produces a folder of animation files in the current directory.
-    if animate:
+        if not animate:
+            print("Files are in sorted folders.")
+            return
+
+        # Produces a folder of animation files in the main output directory.
         dirs = [f for f in scandir(dir) if f.is_dir()]
-        diroutname = path.basename(path.dirname(dir)) + "_" + filetype + "_output"
+
         if len(dirs) > 0:
             diroutpath = path.join(animator_output_path, diroutname)
             if not path.isdir(diroutpath): mkdir(diroutpath)
             for d in dirs:
                 filelist_test = [f.name for f in scandir(d) if isfile(join(d, f.name)) and f.name.split(".")[-1] in image_file_types]
+
+                # Creates animation file if directory's contents are numbered frames.
                 if len(filelist_test) > 2:
-                    outpath = path.join(diroutpath, d.name + "." + filetype)
-                    create_animation_file(d.path, d.name, framerate, frames, filetype, starting_frame, mirror_list, reverse, outpath)
+                    create_animation_file(d.path, d.name, framerate, frames, filetype, starting_frame, mirror_list, reverse, diroutpath, info)
                 else:
                     print("No appropriate file list in %s" % (d))
 
@@ -238,8 +259,33 @@ def get_frame_input(amt, max):
 
     return starting_frame
 
-def create_animation_file(dirpath, dirname, framerate, frames, filetype, starting_frame, mirror_list, reverse, outpath):
+def set_valid_filename(filepath, basename, filetype, i):
+    if i > 0:
+        newname = "%s(%d).%s" % (basename, i, filetype)
+    else:
+        newname = basename
+
+    filename = path.join(filepath, newname)
+    if isfile(filename):
+        return set_valid_filename(filepath, basename, filetype, i + 1)
+    return filename
+
+# Checks if file exists before setting animator
+def confirm_file_changes(outpath):
+    if isfile(outpath):
+        confirmed = input("\nAnimation file found: %s\n\nDo you wish to make another? (y/n) : " % outpath)
+        if confirmed.strip().lower() in ("y", "yes"):
+            return True
+
+        return False
+
+    # Safe to write file
+    return True
+
+def create_animation_file(dirpath, dirname, framerate, frames, filetype, starting_frame, mirror_list, reverse, diroutpath, info):
+    filename = dirname
     files = []
+
     for f in scandir(dirpath):
         if isfile(f.path):
             checkedname = get_filename(f.name)
@@ -250,25 +296,58 @@ def create_animation_file(dirpath, dirname, framerate, frames, filetype, startin
 
     # this is to fix file paths that include Windows styled paths and apostrophes
     files = [escape_str(f) for f in files]
-    frames_ready = False
 
+    filename += "." + filetype
+
+    frames_ready = False
+    outpath = path.join(diroutpath, filename)
+    # ask to overwrite before new frames are set
+    if not confirm_file_changes(outpath):
+        print("Skipping animation: %s" % outpath)
+        return
+
+    # using default settings lets user select frames
     if not frames and not starting_frame:
         animator = MLAnimator(dirname, length=len(files))
         frames_ready = animator.set_valid_frames()
+
+    # non default sets any uninitialized setting to its max amount
     else:
         if frames == None:
             frames = len(files)
+            if starting_frame:
+                frames = int(frames) - int(starting_frame)
+
         if starting_frame == None:
-            starting_frame = len(files) - int(frames)
+            starting_frame = len(files) - int(frames) + 1
 
         animator = MLAnimator(dirname, int(starting_frame), len(files), int(frames))
         frames_ready = animator.check_valid_frames()
-        if not frames_ready:
-            frames_ready = animator.set_valid_frames()
 
+        # if frames didn't validate, ask to manually set
+        if not frames_ready:
+            select = input("Set new frame values for %s? (y/n) : " % self.name)
+            if select.strip().lower() in ("y", "yes", ""):
+                frames_ready = animator.set_valid_frames()
+
+    # return if user canceled out of setting frames loop
     if not frames_ready:
-        print("Skipping animation of %s" % (dirname))
+        print("Skipping animation of %s." % (animator.name))
         return
+
+    # attach frame settings to filename if --info is set
+    if info:
+        filename = "%s(sf%d_f%d_fr%d).%s" % (dirname, animator.starting_frame, animator.frames, framerate, filetype)
+        outpath = path.join(diroutpath, filename)
+        print("outpath:",outpath)
+
+        # ask to overwrite before creating duplicate animation
+        if not confirm_file_changes(outpath):
+            print("Skipping animation: %s" % animator.name)
+            return
+
+    outpath = set_valid_filename(diroutpath, filename, filetype, 0)
+    # outpath = path.join(diroutpath, filename)
 
     starting_frame = animator.starting_frame - 1
     end_frame = animator.frames + starting_frame - 1
@@ -285,12 +364,12 @@ def create_animation_file(dirpath, dirname, framerate, frames, filetype, startin
         for image in reversed_list[:-1]:
             txtfile.write("file \'"+image+"\'\n")
 
-    listpath = path.join(dirpath, "filelisttoanimation.txt")
-    listpath = escape_str(listpath)
+    listpath = escape_str(path.join(dirpath, "filelisttoanimation.txt"))
 
+    print("Animating: %s\nStarting frame %d\nEnd Frame: %d\nFile List Length: %d\n\nSaving file to %s" % (animator.name, animator.starting_frame, animator.starting_frame + animator.frames, animator.length, outpath))
     outpath = escape_str(outpath)
-    print("Animating", outpath, "from frame", str(animator.starting_frame), "to frame", str(animator.starting_frame + animator.frames), "from a file list of", str(animator.length))
-    cmdargs = ['ffmpeg', '-hide_banner', '-loglevel', 'error', '-r', str(framerate), '-f', 'concat', '-safe', "0", '-i', listpath, outpath]
+
+    cmdargs = ['ffmpeg', '-hide_banner', '-loglevel', 'error', '-y', '-r',  str(framerate), '-f', 'concat', '-safe', "0", '-i', listpath, outpath]
     subprocess.call(cmdargs)
 
 def escape_str(a_str):
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # MLAnimation
- Repo for storing the files I use to make animations with BigSleep and DeepDaze. WIP
-
- Use this colab to make DeepDaze renders with a base image. Uses the MLAnimation.py to be able to sort and animate the frames.
+ Repo for storing the files I use to make animations with lucidrains's BigSleep and DeepDaze projects.
+ `MLAnimator.py` sorts the output folder of BigSleep or DeepDaze and using FFMPEG can animate the results easily.
+ 
+ BigSleep: https://github.com/lucidrains/big-sleep
+ DeepDaze: https://github.com/lucidrains/deep-daze
+ FFMPEG: https://www.ffmpeg.org/
+ 
+ CoLab Notebook: https://colab.research.google.com/drive/12EU4iVue3I91Pfqo5hc0-gXOtc81e3kK?usp=sharing
+ 
+ How to use:
+ 
+	Open the command window at the directory where you wish to store the output folder. Call `MLAnimator.py` in python with the argument `-dir` set to the directory that contains the unsorted image output (or the directory that contains the sorted folders after using MLAnimator on it.)
+	
+	The program will ask you for a starting frame and an amount of frames to render. If you skip setting a starting frame, the frames will be chosen from the highest numbered files. If you do set a starting frame, the frames will be chosen starting with that image and selecting higher numbered frames. 
+	You can also supply the starting frame and amount of frames with the arguments "-sf <number>" and "-f <number>". Use "-h" to see all the arguments available.
+	
+	`MLAnimator.py` is also in the CoLab notebook, but you may want to make a zipped copy of your files before animating in case your runtime limit is hit.
+	
+	
+Known Issue:
+	If your path for `-dir` ends with a backslash and quotes `\"`, the program cannot run. Just remove the ending backslash.
+	
+I'm planning on updating this repo and making more in depth tutorials for machine learning projects soon!
